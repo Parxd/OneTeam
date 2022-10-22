66f41715c0a26869876618139e7e7f8043635886
Daniel Park <102764808+Parxd@users.noreply.github.com>
2022-10-22 17:16:07 -0500

    Update LoginForm.js

--- frontend/src/components/login/LoginForm.js
+++ frontend/src/components/login/LoginForm.js
@@ -86,7 +86,7 @@ const LoginForm = () => {
             <div class="p-2 w-full">
             <button
               onClick={login} 
-              class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+              class="flex mx-auto text-white bg-blue-500 border-0 py-2.5 px-8 focus:outline-none hover:bg-blue-600 rounded text-base">Login</button>
             </div>
         </div>
         </div>
