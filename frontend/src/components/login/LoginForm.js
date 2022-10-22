ab368730ea27f69dc4e944edff0ce933d1508d99
Daniel Park <102764808+Parxd@users.noreply.github.com>
2022-10-22 16:45:45 -0500

    Add login w/ email & password functionality

--- frontend/src/components/login/LoginForm.js
+++ frontend/src/components/login/LoginForm.js
@@ -1,10 +1,16 @@
 import React from "react";
-
-import { GoogleButton } from "react-google-button";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../../firebase";
 
+// Imports for Google sign-in functionality
+import { GoogleButton } from "react-google-button";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+// Imports for email & password login functionality
+import { useEffect, useState } from "react";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+
 const LoginForm = () => {
+  // Google sign-in login functionality
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
@@ -13,13 +19,35 @@ const LoginForm = () => {
 
   const handleGoogleSignIn = async () => {
     try {
-      await googleSignIn();
+      googleSignIn();
     } catch (error) {
       console.log(error);
     }
   };
 
+  // Email & Password login functionality
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  });
+  const login = async () => {
+    try {
+      const user = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      console.log(user);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
+  // Page structure
   return (
     <section class="text-gray-400 bg-gray-900 body-font relative">
     <div class="container px-5 py-24 mx-auto">
@@ -27,18 +55,38 @@ const LoginForm = () => {
         <div class="flex flex-wrap -m-2">
             <div class="p-2 w-1/2">
             <div class="relative">
-                <label for="name" class="leading-7 text-sm text-gray-400">Name</label>
-                <input type="text" id="name" name="name" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+                <label 
+                  for="Email"
+                  class="leading-7 text-sm text-gray-400">Email</label>
+                <input 
+                  type="text" 
+                  id="Email" 
+                  name="Email" 
+                  onChange={(event) => {
+                    setLoginEmail(event.target.value);
+                  }}
+                  class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
             </div>
             </div>
             <div class="p-2 w-1/2">
             <div class="relative">
-                <label for="email" class="leading-7 text-sm text-gray-400">Email</label>
-                <input type="email" id="email" name="email" class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
+                <label 
+                  for="Password" 
+                  class="leading-7 text-sm text-gray-400">Password</label>
+                <input 
+                  type="text" 
+                  id="Password" 
+                  name="Password" 
+                  onChange={(event) => {
+                    setLoginPassword(event.target.value);
+                  }}
+                  class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-blue-500 focus:bg-gray-900 focus:ring-2 focus:ring-blue-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></input>
             </div>
             </div>
             <div class="p-2 w-full">
-            <button class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+            <button
+              onClick={login} 
+              class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
             </div>
         </div>
         </div>
